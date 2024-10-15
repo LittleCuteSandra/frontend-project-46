6736eebacdcc6037702b0b542f3bc08256319778
Королева Александра <sasha.sasha.koroleva@mail.ru>
2024-10-15 18:07:54 +0300

    change src/formatters/index.js and create src/formatters/json.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,13 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js';
 
 const formOutput = (format, data) => {
   if (format === 'plain') {
     return plain(data);
+  }
+  if (format === 'json') {
+    return json(data);
   }
   return stylish(data);
 };
---
+++ src/formatters/json.js
@@ -0,0 +1,21 @@
+const getJSONFormat = (data) => {
+  const dataToObj = data.map((node) => {
+    const negativeKey = `-${node.key}`;
+    const positiveKey = `+${node.key}`;
+    switch (node.status) {
+      case 'deleted':
+        return { [negativeKey]: node.value };
+      case 'added':
+        return { [positiveKey]: node.value };
+      case 'changed':
+        return { [negativeKey]: node.value1, [positiveKey]: node.value2 };
+      case 'nested':
+        return { [node.key]: getJSONFormat(node.children) };
+      default:
+        return { [node.key]: node.value };
+    }
+  });
+  return JSON.stringify(dataToObj);
+};
+
+export default getJSONFormat;
