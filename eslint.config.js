e71f3ff5fc0ad4f7aa412bfe6432a66caec62641
Королева Александра <sasha.sasha.koroleva@mail.ru>
2024-10-16 17:00:27 +0300

    modified environment: package-lock.json package.json eslint.config.js .npmrc

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import importPlugin from 'eslint-plugin-import';
+
+
+export default [
+  { languageOptions:
+    {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals:
+      {
+        ...globals.node,
+        ...globals.jest
+      },
+    },
+    plugins: { import: importPlugin },
+  },
+  {
+    rules: {
+      'import/extensions': [
+        'error',
+        {
+          js: 'always',
+        },
+      ],
+      'no-console': 'off',
+    },
+  },
+  pluginJs.configs.recommended,
+];
